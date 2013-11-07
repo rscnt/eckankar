85a26ec18203a1732fd19e77488d15c1b056b0eb
rscnt <raul_ascencio@lavabit.com>
2013-11-06 19:28:14 -0600

    repeat and loop, and others buttons

--- src/main/webapp/app/base.playlist.js
+++ src/main/webapp/app/base.playlist.js
@@ -12,13 +12,17 @@ base.playlist = {
 		$("#jplayer").jPlayer("volume", 0.50);
 		base.playlist.bindEvents();
 	},
+	status : {
+		loop : false,
+		random : false
+	},
 	stackPlaylist : new Array(), // [{el: $(".song-playlist), cancion: {} id:
 	// 0}]
 	currentPlaylist : {
 		el : null,
 		cancion : null,
 		id : null,
-		fav: false,
+		fav : false,
 		fname : null
 	},
 	addtoPlaylist : function(cancion) {
@@ -62,6 +66,32 @@ base.playlist = {
 		base.playlist.showCurrentSong();
 		base.playlist.playSong(base.playlist.currentPlaylist.cancion.fName);
 	},
+	playNext : function() {
+		if (base.playlist.currentPlaylist.id === (base.playlist.stackPlaylist.length - 1)) {
+			base.playlist.currentPlaylist = base.playlist.stackPlaylist[0];
+		} else {
+			base.playlist.currentPlaylist = base.playlist.stackPlaylist[base.playlist.currentPlaylist.id + 1];
+		}
+		base.playlist.playCurrent();
+		return true;
+	},
+	playPrevious : function() {
+		if (base.playlist.currentPlaylist.id === 0) {
+			base.playlist.currentPlaylist = base.playlist.stackPlaylist[base.playlist.stackPlaylist.length - 1];
+		} else {
+			base.playlist.currentPlaylist = base.playlist.stackPlaylist[base.playlist.currentPlaylist.id - 1];
+		}
+		base.playlist.playCurrent();
+		return true;
+	},
+	playRandom : function() {
+		var s = Math.floor(Math.random()
+				* (base.playlist.stackPlaylist.length - 0 + 1) + 0);
+		console.log('S : ' + s);
+		base.playlist.currentPlaylist = base.playlist.stackPlaylist[s];
+		base.playlist.playCurrent();
+		return true;
+	},
 	showCurrentSong : function() {
 		Messenger()
 				.post(
@@ -78,40 +108,16 @@ base.playlist = {
 
 	},
 	bindEvents : function() {
-		$(".jp-next")
-				.click(
-						function(e) {
-							if (base.playlist.currentPlaylist.id === (base.playlist.stackPlaylist.length - 1)) {
-								base.playlist.currentPlaylist = base.playlist.stackPlaylist[0];
-							} else {
-								base.playlist.currentPlaylist = base.playlist.stackPlaylist[base.playlist.currentPlaylist.id + 1];
-							}
-							console.log('-- next --');
-							console.log(base.playlist.currentPlaylist.cancion);
-							console.log('-- next --');
-							base.playlist.playCurrent();
-							e.preventDefault();
-							return false;
-						});
-		$(".jp-previous")
-				.click(
-						function(e) {
-							if (base.playlist.currentPlaylist.id === 0) {
-								console
-										.log(base.playlist.stackPlaylist[base.playlist.stackPlaylist.length - 1]);
-								base.playlist.currentPlaylist = base.playlist.stackPlaylist[base.playlist.stackPlaylist.length - 1];
-							} else {
-								base.playlist.currentPlaylist = base.playlist.stackPlaylist[base.playlist.currentPlaylist.id - 1]; // never
-								// go
-								// never
-							}
-							console.log('-- previous --');
-							console.log(base.playlist.currentPlaylist.cancion);
-							console.log('-- previous --');
-							base.playlist.playCurrent();
-							e.preventDefault();
-							return false;
-						});
+		$(".jp-next").click(function(e) {
+			base.playlist.playNext();
+			e.preventDefault();
+			return false;
+		});
+		$(".jp-previous").click(function(e) {
+			base.playlist.playPrevious();
+			e.preventDefault();
+			return false;
+		});
 
 		$(".jp-show-plst").click(function(e) {
 			e.preventDefault();
@@ -132,8 +138,43 @@ base.playlist = {
 		});
 
 		$(".jp-info").click(function(e) {
+			e.preventDefault();
 			base.playlist.showCurrentSong();
 		});
 
+		$(".jp-loop").click(
+				function(e) {
+					if (base.playlist.status.loop) {
+						$("#jplayer").unbind(".repeat");
+						base.playlist.status.loop = false;
+						$(".jp-loop").removeClass('active');
+					} else {
+						$(".jp-loop").addClass('active');
+						base.playlist.status.loop = true;
+						$("#jplayer").bind($.jPlayer.event.ended + ".repeat",
+								function() {
+									base.playlist.playNext();
+								});
+					}
+					e.preventDefault();
+				});
+
+		$(".jp-random").click(
+				function(e) {
+					if (base.playlist.status.random) {
+						$("#jplayer").unbind(".random");
+						base.playlist.status.random = false;
+						$(".jp-random").removeClass('active');
+					} else {
+						$(".jp-random").addClass('active');
+						base.playlist.status.random = true;
+						$("#jplayer").bind($.jPlayer.event.ended + ".random",
+								function() {
+									console.log('Random BABY');
+									base.playlist.playRandom();
+								});
+					}
+					e.preventDefault();
+				});
 	}
 }
